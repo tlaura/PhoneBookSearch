f5930f25fd1077ec2ea4ab1da9ad4a7ee3ece4b4
Laura <tundelaura93@gmail.com>
2019-12-03 16:29:15 +0100

    refactor main

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> list =  new ArrayList<>();
         File file = new File("/Users/tundelaura/Desktop/java/phoneBook/directory.txt");
         File find = new File("/Users/tundelaura/Desktop/java/phoneBook/find.txt");
         Stats stat = new Stats(file, find);
-//        stat.linearSearch();
+        stat.linearSearch();
         System.out.println();
-//        stat.bubbleAndJump();
         stat.hashMapSearch();
-
-//        Scanner scanner = new Scanner(System.in);
-//        list.add("Lyndel Raymond");
-//        list.add("Meta Joappa");
-//        list.add("Kynthia Nadbus");
-//        list.add("Ethelind Skeie");
-//        list.add("Shoshana Utica");
-//        list.add("Marisa Firman");
-//        list.add("Gwenette Anagnos");
-//
-//        Sort sort = new Sort();
-//        sort.quickSort(list, 0, list.size()-1);
-//
-//        for (String s: list) {
-//            System.out.println(s);
-//        }
+        System.out.println();
+        stat.quickSortAndBinarySearch();
+//        stat.bubbleAndJump();
     }
 }
